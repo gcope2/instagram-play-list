cd47eac56e6d25e4d519026e9f6330f38f3a7966
Gabby Cope <gtc5173@psu.edu>
2026-03-30 08:46:35 -0400

    pt4?

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,11 @@ const baseConfig = createBasicConfig({
   outputDir: 'public'
 });
 
+// Override to use consistent filename instead of hash
+baseConfig.output.entryFileNames = 'instagram-play-list.js';
+baseConfig.output.chunkFileNames = 'instagram-play-list-[name].js';
+baseConfig.output.assetFileNames = 'instagram-play-list-[name][extname]';
+
 export default {
   ...baseConfig,
   input: 'instagram-play-list.js',
